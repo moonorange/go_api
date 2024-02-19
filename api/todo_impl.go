b299d2dfaf8a7b79df773a741466582d720d4fbb
moonorange <monoma632@gmail.com>
2024-02-19 19:25:56 +0530

    add: task read

--- api/todo_impl.go
+++ api/todo_impl.go
@@ -6,13 +6,12 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
-	"github.com/moonorange/go_api/domain"
 	"github.com/moonorange/go_api/domain/services"
 )
 
 type (
 	TodoStore struct {
-		TODOs map[domain.TaskId]domain.Todo
+		TODOs map[string]Task
 		Lock  sync.Mutex
 	}
 	todoAPI struct {
@@ -28,13 +27,13 @@ func NewTodoAPI(service services.TodoService) ServerInterface {
 	return &todoAPI{
 		s: service,
 		Store: &TodoStore{
-			TODOs: make(map[domain.TaskId]domain.Todo)},
+			TODOs: make(map[string]Task)},
 	}
 }
 
 // TasksCreate implements ServerInterface.
 func (t *todoAPI) TasksCreate(w http.ResponseWriter, r *http.Request) {
-	var newTODO domain.Todo
+	var newTODO Task
 	err := json.NewDecoder(r.Body).Decode(&newTODO)
 	if err != nil {
 		sendTodoError(w, http.StatusBadRequest, "Invalid format for newTODO")
@@ -43,8 +42,8 @@ func (t *todoAPI) TasksCreate(w http.ResponseWriter, r *http.Request) {
 	t.Store.Lock.Lock()
 	defer t.Store.Lock.Unlock()
 
-	newID := domain.TaskId(uuid.New())
-	newTODO.ID = newID
+	newID := uuid.New().String()
+	newTODO.Id = &newID
 	t.Store.TODOs[newID] = newTODO
 
 	w.WriteHeader(http.StatusCreated)
@@ -61,7 +60,7 @@ func (t *todoAPI) TasksGetAll(w http.ResponseWriter, r *http.Request) {
 	t.Store.Lock.Lock()
 	defer t.Store.Lock.Unlock()
 
-	var tasks []domain.Todo
+	var tasks []Task
 
 	for _, todo := range t.Store.TODOs {
 		// Add all pets if we're not filtering
@@ -74,7 +73,17 @@ func (t *todoAPI) TasksGetAll(w http.ResponseWriter, r *http.Request) {
 
 // TasksRead implements ServerInterface.
 func (t *todoAPI) TasksRead(w http.ResponseWriter, r *http.Request, taskId string) {
-	panic("unimplemented")
+	t.Store.Lock.Lock()
+	defer t.Store.Lock.Unlock()
+
+	v, ok := t.Store.TODOs[taskId]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(nil)
+		return
+	}
+
+	_ = json.NewEncoder(w).Encode(v)
 }
 
 // TasksUpdate implements ServerInterface.
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/getkin/kin-openapi v0.122.0
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/google/uuid v1.5.0
+	github.com/labstack/gommon v0.4.2
 	github.com/oapi-codegen/nethttp-middleware v1.0.1
 	github.com/oapi-codegen/runtime v1.1.1
 )
@@ -18,7 +19,12 @@ require (
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
